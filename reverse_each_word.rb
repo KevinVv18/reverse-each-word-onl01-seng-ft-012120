74f0183d5a2dd53d27ba8105545611ff0f2444f7
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-26 21:00:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(string)
-  r_array= string.split("")
+  o_array= string.split("")
+  r_array= []
+  
+  o_array.each do |string|
+    r_array<< o_array.reverse
+  end
+  
   
 
   
