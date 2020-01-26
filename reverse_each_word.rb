1c1fcd8cf6af8893f39982453aaa82c016c3e811
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-26 20:56:49 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,6 @@
-def reverse_each_word(sentence1)
-  array= []
-  
-  array<< "#{sentence1}"
-array.each do |array|
-  array.reverse!
-end
-array.reverse!
 
 
-end
+
+ reverse_each_word(string)
   
+end
